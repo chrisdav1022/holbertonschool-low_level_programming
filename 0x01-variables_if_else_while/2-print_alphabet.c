73cf11957274569ff2f6b447aa4c7af9c0d26d1f
chrisdav1022 <chris_bedoya01@hotmail.com>
2020-06-11 22:20:42 +0000

    current file

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,14 +3,15 @@
 /**
  * main - entry point
  *
- *Return: 0
+ *Return: always 0.
  */
 int main(void)
 {
 	char n;
 
 	for (n = 'a'; n <= 'z'; n++)
-		putchar(n);
+
+	putchar(n);
 
 	putchar('\n');
 	return (0);
