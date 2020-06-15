298553480d8942fba6ffc043d6590f430e5d55bf
chrisdav1022 <chris_bedoya01@hotmail.com>
2020-06-15 05:27:55 +0000

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * print_alphabet - print the alphabet
+ * 
+ */
+void print_alphabet(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
