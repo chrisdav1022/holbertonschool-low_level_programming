872df6102ad352744f229b96825326ea967408ff
chrisdav1022 <chris_bedoya01@hotmail.com>
2020-06-16 20:46:29 +0000

    x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ *print_alphabet_x10 - print 10 alphabet.
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int n, abc;
+
+	for (n = 0; n <= 9; n++)
+		for (abc = 'a'; abc <= 'z'; abc++)
+			_putchar(abc);
+
+	_putchar ('\n');
+}
