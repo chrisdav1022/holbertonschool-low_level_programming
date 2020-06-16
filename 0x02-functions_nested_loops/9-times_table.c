3b303ad65dcae5453c95ffd5e8f46a7413c06c54
chrisdav1022 <chris_bedoya01@hotmail.com>
2020-06-16 21:32:55 +0000

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+
+/**
+ *times_table - Entry point
+ *
+ *Return: Returns 0 upon successful run.
+ */
+
+void times_table(void)
+{
+	int n, n1, nm;
+
+	for (n = 0; n <= 9; n++)
+	{
+		for (n1 = 0; n1 <= 9; n1++)
+		{
+			nm = n * n1;
+			if (n1 == 0)
+			{
+				_putchar(mn + '0');
+			}
+			else if (mn <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(mn + '0');
+			}
+			else if (mn >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(mn / 10 + '0');
+				_putchar(mn % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
