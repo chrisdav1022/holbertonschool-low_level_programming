e97665900cb9433560c172a1a8c489f50d43272a
chrisdav1022 <chris_bedoya01@hotmail.com>
2020-06-16 21:24:52 +0000

    las digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * print_last_digit - print last digit
+ *
+ * @n: my number
+ *
+ * Return: Returns 0.
+ */
+int print_last_digit(int n)
+{
+	int c;
+
+	c = n % 10;
+
+	if (n < 0)
+		c = c * -1;
+
+	_putchar(c + '0');
+
+	return (c);
+}
