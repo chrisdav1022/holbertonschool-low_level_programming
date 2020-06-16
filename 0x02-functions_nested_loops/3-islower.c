df6ee1bdd0f9cfdf31dfd823d50bdc179fb2b2fc
chrisdav1022 <chris_bedoya01@hotmail.com>
2020-06-16 20:57:56 +0000

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * _islower - print islower to z or a
+ * @c: int c
+ * Return: return 1 to if and 0 run.
+ */
+int _islower(int c)
+{
+	int c;
+
+	if (c <= 'z' && c >= 'a')
+		return (1);
+	return (0);
+}
