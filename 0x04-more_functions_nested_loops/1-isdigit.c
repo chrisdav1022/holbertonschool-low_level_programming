c5f25d6e9c2004c55675cbd00597411467eef926
chrisdav1022 <chris_bedoya01@hotmail.com>
2020-06-18 19:57:38 +0000

    digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * _isdigit - print 1 if is digit
+ * @c: contains digit 1 and 0 if is alpha
+ * Return: return 0 and 1
+ */
+int _isdigit(int c)
+{
+	if (c >= 'a')
+		return (0);
+
+	return (1);
+}
