4612dfb72af127eba0ef3802ce5f3f4cd722c872
chrisdav1022 <chris_bedoya01@hotmail.com>
2020-06-18 20:17:01 +0000

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * print_numbers - print numbers from 1 to 9.
+ *
+ * Return: none
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+		putchar(n);
+	putchar('\n');
+}
