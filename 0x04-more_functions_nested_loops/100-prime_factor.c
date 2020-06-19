92a51687303dbb1a3773bf66cb6776da94540eca
chrisdav1022 <chris_bedoya01@hotmail.com>
2020-06-19 05:13:46 +0000

    largest prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ *main - Entry point
+ *
+ *Return: (0) Always
+ */
+
+int main(void)
+{
+	long c, n1;
+
+	n = 612852475143;
+
+	for (n1 = 2; n > n1; n1++)
+	{
+		while (n % n1 == 0)
+		{
+			n = n / n1;
+		}
+	}
+	printf("%lu", n1);
+	putchar('\n');
+	return (0);
+}
