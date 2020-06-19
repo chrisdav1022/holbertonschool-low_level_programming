87196c03a4bcb2bb8d4a43a8fe6096a53a7d9bf7
chrisdav1022 <chris_bedoya01@hotmail.com>
2020-06-19 03:50:20 +0000

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ * main - FizzBuzz
+ *
+ * Return: none
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 1; c <= 100; c++)
+	{
+		if ((c % 3 == 0) && (c % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if (c % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (c % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (c == 100)
+			printf("Buzz");
+		else
+		{
+			printf("%d ", c);
+		}
+	}
+	printf("\n");
+	return (0);
+}
