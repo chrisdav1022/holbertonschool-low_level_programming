14deb0b9281712d851310d9709a4afe1ea007f0d
chrisdav1022 <chris_bedoya01@hotmail.com>
2020-06-23 19:18:15 +0000

    0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,10 @@
 #include "holberton.h"
+
 /**
- * reset_to_98 - reset in num 98
- * @n: pointer 98
- * Return: always 0
+ * reset_to_98 - Update the value n points to to 98
+ * @n: Pointer to an int to update
  */
-int reset_to_98(int *n)
+void reset_to_98(int *n)
 {
-
 	*n = 98;
-
 }
