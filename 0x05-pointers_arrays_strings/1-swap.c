2b6f7dd39862b76104aaa1bdc077123deae52b7c
chrisdav1022 <chris_bedoya01@hotmail.com>
2020-06-23 08:21:34 +0000

    1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,1 +1,16 @@
 #include "holberton.h"
+
+/**
+ * swap_int - intercambia el valor de los enteros
+ *@a: valor que se cambia a b
+ *@b: valor que se cambia a a
+ */
+
+void swap_int(int *a, int *b)
+{
+	int y;
+
+	y = *a;
+	*a = *b;
+	*b = y;
+}
