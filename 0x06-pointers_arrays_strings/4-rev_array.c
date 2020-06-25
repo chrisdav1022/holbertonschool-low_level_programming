b8f26f585d0c8e747cdcaa3ea3c795b07d40b07f
chrisdav1022 <chris_bedoya01@hotmail.com>
2020-06-25 21:57:36 +0000

    revert

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,13 +9,14 @@
 
 void reverse_array(int *a, int n)
 {
-	int y;
-	int c;
+	int y = 0;
+	int c = 0;
 
+	n = n -1;
 	for (y = 0; y <= n; y++)
 	{
 		c = a[y];
-		a[y] = a[n - 1];
+		a[y] = a[n];
 		a[n] = c;
 		n--;
 	}
