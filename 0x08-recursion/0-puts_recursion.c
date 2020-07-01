6ea1337b8838f706b334df11188e40a80a5bfa00
chrisdav1022 <chris_bedoya01@hotmail.com>
2020-07-01 23:45:07 +0000

    current file

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,1 +1,21 @@
 #include "holberton.h"
+/**
+ * _puts_recursion - recursion to betty holb
+ *
+ * @s: pointer to puts recursion
+ */
+void _puts_recursion(char *s)
+{
+	if (s[0] != '\0')
+
+	{
+		_putchar(s[0]);
+		s++;
+
+		_puts_recursion(s);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
