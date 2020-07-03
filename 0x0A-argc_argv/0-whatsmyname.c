492fecefcbd2afc61b66537dba08a096c896fcf3
chrisdav1022 <chris_bedoya01@hotmail.com>
2020-07-03 10:38:44 +0000

    current file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - escribe el nombre del atributo
+ *@argc:numero de argumentos
+ *@argv:argumento de programa
+ * Return: devuelve el resultado
+ */
+
+int main(int argc, char *argv[]__attribute__((unused)))
+
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
