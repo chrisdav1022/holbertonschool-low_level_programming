7a372b5f266c71461f4210d131d9e0a821a42f9a
chrisdav1022 <chris_bedoya01@hotmail.com>
2020-07-04 22:41:53 +0000

    current file

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,7 +7,7 @@
  * Return: devuelve el resultado
  */
 
-int main(int argc, __attribute__((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
