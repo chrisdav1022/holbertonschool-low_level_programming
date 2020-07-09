193e63a0abb37b1c5076ecb9e7d9275439e284f7
chrisdav1022 <chris_bedoya01@hotmail.com>
2020-07-09 06:01:19 +0000

    current file

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+include "holberton.h"
+/**
+ * leet - check the code for Holberton School students.
+ *@n: the variable
+ *Return: always 0
+ */
+char *leet(char *n)
+{
+	int c, ch;
+	char le[] = "aeotl";
+	char num[] = "43071";
+
+	for (c = 0; n[c] != '\0'; c++)
+	{
+		for (ch = 0; le[ch] != '\0'; ch++)
+		{
+			if (n[c] == le[ch] || n[c] == le[ch] - 32)
+				n[c] = num[ch];
+		}
+	}
+
+	return (n);
+}
