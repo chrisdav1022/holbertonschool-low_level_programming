e482a06d0990fa2ad95f82194b970e3ad5ab726d
chrisdav1022 <chris_bedoya01@hotmail.com>
2020-07-09 07:31:14 +0000

    current file

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,34 +1,33 @@
 #include "holberton.h"
-
 /**
- *
- *
- *
- *
+ * alloc_grid - check the code for Holberton School students.
+ *@width: the variable
+ *@height: another variable
+ *Return: dest
  */
-
 int **alloc_grid(int width, int height)
 {
-	int **p = NULL, i, j;
-    
-	if (width <= 0 || height <= 0)
-		return(NULL);
+	int **p, c1, c2, c3;
 
-	p = malloc (width * sizeof(int)); //creamos el array principal [][][]...[]
-        if (p == NULL)
-		return(NULL);
-        
-	for (i = 0; i <= width; i++ )
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	p = malloc(height * sizeof(int *));
+
+	if (p == NULL)
+		return (NULL);
+
+	for (c1 = 0; c1 < height; c1++)
 	{
-		p[i] = malloc (height * sizeof(int)); //creamos el array secundario 
-		if (p[i]  == NULL)
-			return(NULL);
-                
-		for (j = 0; j <= height; j++)
-			free(p[j]);
-			p[i][j] = 0;
-		
-		free(p);
+		p[c1] = malloc(width * sizeof(int));
+		if (p[c1] == NULL)
+		{
+			for (c3 = 0; c3 <= c1; c3++)
+				free(p[c3]);
+			free(p);
+			return (NULL);
+		}
+		for (c2 = 0; c2 < width; c2++)
+			p[c1][c2] = 0;
 	}
 	return (p);
 }
