68c14aa0ecbee2c1a14f7b10fef947d33603d82f
chrisdav1022 <chris_bedoya01@hotmail.com>
2020-07-13 23:33:14 +0000

    current file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - print or __FILE__
+ * Return: return print
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,7 @@
+
+#ifndef ABSS
+#define ABSS
+
+#define ABS(X) ((X) >= (0) ? (X) : -(X))
+
+#endif
