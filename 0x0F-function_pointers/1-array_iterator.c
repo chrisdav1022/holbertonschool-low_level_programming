326120d513544990971d5cb0a9ec8d2cfb0bd614
chrisdav1022 <chris_bedoya01@hotmail.com>
2020-07-15 17:40:37 +0000

    current file

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,9 +1,12 @@
 #include "function_pointers.h"
+
 /**
- *
- *
- *
+ * array_iterator - iterator of array
+ * @array: pointer to array
+ * @size: size to size
+ * @action:pointer to function
  */
+
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
