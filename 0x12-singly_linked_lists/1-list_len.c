1aa6653eae70f8da89d9a7cf22db74ac27bcabc0
chrisdav1022 <chris_bedoya01@hotmail.com>
2020-07-23 23:16:56 +0000

    current file

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list_len - print number to elements
+ * @h: pointer to struct
+ * Return: size
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t i;
+
+	if (!h)
+		return (0);
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
