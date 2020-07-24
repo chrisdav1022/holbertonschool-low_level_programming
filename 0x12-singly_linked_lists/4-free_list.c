94212a3bf8290ae202a9b4ad01ec73de97f1c327
chrisdav1022 <chris_bedoya01@hotmail.com>
2020-07-24 03:57:35 +0000

    current file

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_list - free the list
+ * @head: pointer the free
+ * Return: none
+ */
+
+void free_list(list_t *head)
+{
+	list_t *i;
+
+	while (!head)
+	{
+		i = head;
+		head = head->next;
+		free(i->str);
+		free(i);
+	}
+}
