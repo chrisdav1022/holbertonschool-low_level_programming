974076bc25e910208ff5244fe912f0abb4e30eee
chrisdav1022 <chris_bedoya01@hotmail.com>
2020-07-28 00:32:28 +0000

    current file

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,10 +1,19 @@
 #include "lists.h"
 /**
- *
- *
- *
+ * print_listint - print the list integer
+ * @h: pointer to list in the struct
+ * Return: return the value the size
  */
 size_t print_listint(const listint_t *h)
 {
-	int i;
+	size_t i = 0;
+
+	while (h)
+	{
+		if (h->n)
+			printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
 }
