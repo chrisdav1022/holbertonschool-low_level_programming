a55ad3f3b58b6c01202a5f20ffce15569a02ec8c
chrisdav1022 <chris_bedoya01@hotmail.com>
2020-07-31 03:24:48 +0000

    current file

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,11 +1,15 @@
-#include "holberrton.h"
+#include "holberton.h"
 /**
- *
- *
- *
+ * get_bit - print binary.
+ *@n: variable
+ *@index: index
+ *Return: counter
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-
-
+	if (index > 63)
+		return (-1);
+	if (index == 0)
+		return (n & 1);
+	return (get_bit(n >> 1, index - 1));
 }
--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,11 +1,18 @@
 #include "holberton.h"
+
 /**
- *
- *
- *
+ * set_bit - set of bit to bit
+ * @n: pointer to numbers
+ * @index: value to bit
+ * Return: return value -1
  */
+
 int set_bit(unsigned long int *n, unsigned int index)
 {
-
-
+	if (index < 63)
+	{
+		*n = *n | (1 << index);
+		return (1);
+	}
+	return (-1);
 }
