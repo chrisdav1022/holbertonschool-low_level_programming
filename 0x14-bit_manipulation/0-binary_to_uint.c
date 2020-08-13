f4d406953582988cac0b483e3c2247b1915c890b
chrisdav1022 <chris_bedoya01@hotmail.com>
2020-08-13 02:15:25 +0000

    current file

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,18 +6,18 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i = 0;
+	unsigned int data = 0;
 
 	if (b == NULL)
 		return (0);
 	while (*b != '\0')
 	{
-		i = i << 1;
+		data = data << 1;
 		if (*b != '1' && *b != '0')
 			return (0);
 		else if (*b == '1')
-			i = i | 1;
+			data = data | 1;
 		b++;
 	}
-	return (i);
+	return (data);
 }
