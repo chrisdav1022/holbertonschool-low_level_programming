71850aac6d5b7889866f1afb7b911f9a3e3f973a
chrisdav1022 <chris_bedoya01@hotmail.com>
2020-09-06 20:45:34 +0000

    current file

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,9 +1,11 @@
 #include "lists.h"
+
 /**
  * print_dlistint - print double linkked list.
  * @h: pointer to double linked list.
  * Return: return node to linked list.
  */
+
 size_t print_dlistint(const dlistint_t *h)
 {
 	const dlistint_t *tmp = h;
