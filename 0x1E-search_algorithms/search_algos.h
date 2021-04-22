d8ae7d32ea53422fb211a0aa221f485bfe7f4188
christian bedoya <chrisdav1022>
2021-04-22 01:57:46 +0000

    current file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,15 @@
+#ifndef ALGOS_H
+#define ALGOS_H
+
+/**
+ *libraries
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
